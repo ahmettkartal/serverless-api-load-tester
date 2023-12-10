75d3c5d94d04d4cabacdada3de21fc1275466d85
ahmettkartal <ahmettkartal@yahoo.com>
2023-12-10 17:36:58 +0300

    request config improved

--- src/consumers/LoadTestConsumer.js
+++ src/consumers/LoadTestConsumer.js
@@ -7,12 +7,19 @@ module.exports.handler = async (event) => {
 
         for (let record of records) {
             let body = JSON.parse(record.body)
-            promises.push(axios.get(body.target_url))
+            promises.push(axios.request({
+                method: body.method,
+                url: body.url,
+                data: JSON.stringify(body.data),
+                headers: body.headers
+            }))
         }
 
-        Promise.all(promises).then((result) => {
-            console.log(result)
-            resolve(result)
+        Promise.all(promises).then((results) => {
+            for (let result of results) {
+                console.log({"data": result.data, status: result.status})
+            }
+            resolve(results)
         }).catch((err) => {
             console.log("err" + err)
             reject(err)
--- src/controllers/LoadTesterController.js
+++ src/controllers/LoadTesterController.js
@@ -4,7 +4,7 @@ const {push} = require("../services/SQSService");
 exports.create = (req, res) => {
     let promises = [];
     for (let i = 0; i < req.body.iteration; i++) {
-        promises.push(push(process.env.LOAD_TEST_SQS_QUEUE_URL, req.body));
+        promises.push(push(process.env.LOAD_TEST_SQS_QUEUE_URL, req.body.request_data));
     }
 
     Promise.all(promises).then((result) => {
